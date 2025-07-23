a9574db89eb7cc1438fc0f2e7c0586e968d96cfd
abhijayanoop-elcarreira <abhijay@elcarreira.com>
2025-07-23 15:50:48 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Creating a Node Server
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views"); // by default it is views, use only if the html files are in a folder with name other than views
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing the incoming req body
+app.use(express.static(path.join(__dirname, "public"))); //middleware for serving files statically (stylesheet in shop.html)
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
