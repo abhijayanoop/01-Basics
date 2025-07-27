b0b1cdeeaf47ba4b6ff035c4f4f3740c4f8af230
abhijayanoop-elcarreira <abhijay@elcarreira.com>
2025-07-27 17:06:42 +0530

    deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,10 @@ exports.postAddProduct = (req, res, next) => {
     imageUrl: imageUrl,
     description: description,
   })
-    .then((result) => console.log(result))
+    .then((result) => {
+      console.log("ADDED PRODUCT");
+      res.redirect("/admin/products");
+    })
     .catch((err) => console.log(err));
 };
 
@@ -64,8 +67,15 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.delete(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
