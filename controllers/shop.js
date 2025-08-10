bd95c0b245d7497736aa5587cf81780c5aba3ef6
abhijayanoop <abhijayanoop007@gmail.com>
2025-08-10 14:43:43 +0530

    Deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -69,15 +69,11 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
-    .then(() => {
+    .removeFromCart(prodId)
+    .then((result) => {
       res.redirect("/cart");
     })
     .catch((err) => console.log(err));
-  // Product.findById(prodId, (product) => {
-  //   Cart.deleteProduct(prodId, product.price);
-  //   res.redirect("/cart");
-  // });
 };
 
 exports.getOrders = (req, res, next) => {
--- models/user.js
+++ models/user.js
@@ -50,4 +50,12 @@ userSchema.methods.addToCart = function (product) {
   return this.save();
 };
 
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((i) => {
+    return i.productId.toString() !== productId;
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
 module.exports = mongoose.model("User", userSchema);
